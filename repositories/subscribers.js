d36122f922099bf77024502fe5ea4d23613738d4
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2018-01-24 16:53:17 +0100

    request user display name

--- repositories/subscribers.js
+++ repositories/subscribers.js
@@ -1,8 +1,21 @@
+const request = require('request-promise');
+
 
 const subscribers = [];
 
-function addSubscribers(subscriber) {
-    subscribers.push(subscriber);
+function addSubscribers(userId) {
+    console.log('client id', process.env.CLIENT_ID);
+    request({
+        uri: `https://api.twitch.tv/kraken/users/${userId}`,
+        headers: {
+            Accept: 'application/vnd.twitchtv.v5+json',
+            'Client-ID': process.env.CLIENT_ID
+        }
+    }).then((response) => {
+        let displayName = JSON.parse(response)['display_name'];
+        subscribers.push(displayName);
+        console.log(displayName);
+    });
 }
 
 function getSubscribers() {
