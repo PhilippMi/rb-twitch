d7c3537a7e112025921ff29e6c22d14ccab3cde5
Julian Fammler <julian.fammler@e-mundo.de>
2018-01-25 09:35:19 +0100

    Changes route

--- routes/twitch-listener.js
+++ routes/twitch-listener.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const {twitchListenerCS} = require('../scripts/twitch-listener');
 
-router.post('/', function(req, res) {
+router.post('/twitch-listener', function(req, res) {
     twitchListenerCS(req.body.data);
     res.status(200).send('ok');
 });
