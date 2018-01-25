843dc4f7349baff7e108656ad8da15847637cb3f
Julian Fammler <julian.fammler@e-mundo.de>
2018-01-25 15:55:44 +0100

    Adds twitch logo

--- scripts/twitch-listener.js
+++ scripts/twitch-listener.js
@@ -1,7 +1,7 @@
 function showStreamLiveBanner(containerEl, channel) {
     const banner = document.createElement('div');
-    banner.className = 'teaser-text teaser-text--image';
-    banner.innerHTML = '<div class="teaser-text__wrapper"><div class="teaser-text__content-container"><div class="teaser-text__expanded"><div class="teaser-text__summary">Red Bull is live on Twitch! <a target="_blank" href="https://www.twitch.tv/redbull"> Click here to watch!</a></div></div></div></div>';
+    banner.className = 'teaser-text teaser-text--image dark-theme';
+    banner.innerHTML = '<div class="teaser-text__wrapper"><div class="twitch-logo"><div class="teaser-text__content-container"><div class="teaser-text__expanded"><div class="teaser-text__summary">Red Bull is live on Twitch! <a target="_blank" href="https://www.twitch.tv/redbull"> Click here to watch!</a></div></div></div></div>';
     containerEl.appendChild(banner);
 }
 
