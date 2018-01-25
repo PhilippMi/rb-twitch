22403e0fabac805bb6e892510d67f8a11d7b807a
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2018-01-25 13:05:06 +0100

    show streams

--- routes/golive.js
+++ routes/golive.js
@@ -4,7 +4,7 @@ const {addStream} = require('../repositories/golive');
 
 router.post('/', function(req, res) {
     let stream = req.body.data;
-    console.log('golive rec', stream);
+    console.log('golive rec', req.body);
     addStream(stream.user_id, stream.title);
     res.status(200).send('ok');
 });
