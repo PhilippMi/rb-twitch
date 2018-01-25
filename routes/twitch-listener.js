86ad3bfc3d96748ce2337fcaf3d17045e7790826
Julian Fammler <julian.fammler@e-mundo.de>
2018-01-25 10:05:23 +0100

    Renders something

--- routes/twitch-listener.js
+++ routes/twitch-listener.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const {getCustomScript} = require('../repositories/twitch-listener');
+const {showTwitch} = require('../repositories/twitch-listener');
 
-router.post('/', function(req, res) {
-    getCustomScript(req.body);
-    res.status(200).send('ok');
-});
-
-router.get('/', function(req, res) {
-    res.send(req.query['hub.challenge']);
+router.get('/', function(req, res, next) {
+    res.render('twitchlistener', { title: 'Twitch', subscribers: showTwitch });
 });
 
 module.exports = router;
