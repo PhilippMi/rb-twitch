be47910c436a58c05917a7cd2ae16be44e409533
Julian Fammler <julian.fammler@e-mundo.de>
2018-01-25 09:44:43 +0100

    Revert twitch route

--- routes/twitch-listener.js
+++ routes/twitch-listener.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const {twitchListenerCS} = require('../scripts/twitch-listener');
+const {addSubscribers} = require('../repositories/subscribers');
 
-router.post('/twitch-listener', function(req, res) {
-    twitchListenerCS(req.body.data);
+router.post('/', function(req, res) {
+    addSubscribers(req.body.data.from_id);
     res.status(200).send('ok');
+});
+
+router.get('/', function(req, res) {
+    res.send(req.query['hub.challenge']);
 });
 
 module.exports = router;
