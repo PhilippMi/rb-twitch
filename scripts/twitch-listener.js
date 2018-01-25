b71441a99efae6eca8efc0536d1eb56cf09b2380
Julian Fammler <julian.fammler@e-mundo.de>
2018-01-25 14:16:11 +0100

    set if condition

--- scripts/twitch-listener.js
+++ scripts/twitch-listener.js
@@ -43,11 +43,10 @@ module.exports = {
     start: ({el}) => {
         return fetch("https://rb-twitch.herokuapp.com/twitch/stream").then(response => {
             const wrapper = document.createElement('div');
-
-            if (true) {
+            console.log(response)
+            if (response.data[0].type === 'live') {
                 wrapper.setAttribute('class', 'test');
                 const div = document.createElement('div');
-                console.log(response.text());
                 response.text().then(data => div.innerHTML = data);
                 const link = document.createElement('a');
                 link.setAttribute.href = 'https://twitch.tv/redbullesports';
